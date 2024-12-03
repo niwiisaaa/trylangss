91bd09e967583e6a772ebf69916e43bb0ddeab02
jaypbazar <jabazar@my.cspc.edu.ph>
2024-12-04 02:09:28 +0800

    TODO: Implement case 4 with sorting

--- try.asm
+++ try.asm
@@ -12,15 +12,16 @@ section .data
     task_number_prompt db "Enter task number: ", 0
     
     display_tasks_msg db 10, "==== Current To-Do List ====", 10, 0
-    task_added_msg db "Task added successfully!", 10, 0
-    task_completed_msg db "Task marked as completed.", 10, 0
-    task_deleted_msg db "Task deleted successfully.", 10, 0
-    task_limit_msg db "Task limit reached. Cannot add more tasks.", 10, 0
-    exit_msg db "Thank you for using the To-Do List Manager!", 10, 0
+    task_added_msg db 10, "Task added successfully!", 10, 0
+    task_completed_msg db 10, "Task %d marked as completed.", 10, 0
+    task_deleted_msg db 10, "Task %d deleted successfully.", 10, 0
+    exit_msg db 10, "Thank you for using the To-Do List Manager!", 10, 0
 
-    no_tasks_msg db "No tasks available.", 10, 0
-    invalid_choice_msg db "Invalid choice. Please try again.", 10, 0
-    invalid_task_number_msg db "Invalid task number. Try again.", 10, 0
+    no_tasks_msg db 10, "No tasks available.", 10, 0
+    task_limit_msg db 10, "Task limit reached! Cannot add more tasks.", 10, 0
+    invalid_choice_msg db 10, "Invalid choice! Please try again.", 10, 0
+    invalid_task_number_msg db 10, "Invalid task number! Try again.", 10, 0
+    already_marked_msg db 10, "Task is already complete!", 10, 0
 
     pending_status db "%d. %s (Pending)", 10, 0
     completed_status db "%d. %s (Completed)", 10, 0
@@ -28,16 +29,16 @@ section .data
     num_format db "%d", 0
     input_format db "%[^.]", 0
 
-    MAX_TASKS equ 8 ; max number of task in the list
+    MAX_TASKS equ 8   ; max number of task in the list
     TASK_SIZE equ 128 ; 127 character limit (excluding null terminator) to each task
 
 section .bss
-    choice resb 1        ; choice is a byte (user input)
+    choice resb 1         ; choice is a byte (user input)
     task resb TASK_SIZE   ; temporary storage for task description
     task_list resb TASK_SIZE * MAX_TASKS  ; array for task descriptions
     task_status resb MAX_TASKS ; status for each task
-    task_count resd 1 ; the current number of tasks
-    task_number resd 1   ; task number input (for completing or deleting)
+    task_count resd 2     ; the current number of tasks
+    task_number resd 2    ; task number input (for completing or deleting)
 
 section .text
     global _main
@@ -112,7 +113,7 @@ _main:
             cmp dword[task_count], 0
             jne proceed_display
 
-            ; display prompt is none
+            ; display message if none
             push no_tasks_msg
             call _printf
             add esp, 4
@@ -123,9 +124,66 @@ _main:
 
             jmp main_loop_start
         case_3:
+            call display_all_task 
+            
+            input_number:
+            ; display prompt for task number
+            push task_number_prompt
+            call _printf
+            add esp, 4
 
+            ; get input for task number to mark as done
+            push task_number
+            push num_format
+            call _scanf
+            add esp, 8
+
+            ; store task number and task count to registers for comparison
+            mov eax, dword[task_number]
+            mov ebx, dword[task_count]
+
+            ; compare if task number is within the task list range
+            cmp eax, 1
+            jl invalid_number
+            cmp eax, ebx
+            jg invalid_number
+
+            ; check if the selected task is already complete
+            dec eax
+            cmp byte[task_status + eax], 1
+            je already_marked
+
+            mov byte[task_status + eax], 1 ; if not, mark task as completed (1)
+            
+            ; display completed task prompt
+            inc eax
+            push eax
+            push task_completed_msg
+            call _printf
+            add esp, 4
+
+            jmp main_loop_start
+
+            invalid_number:
+                ; display prompt and ask for another input
+                push invalid_task_number_msg
+                call _printf
+                add esp, 4
+                
+                call clear_buffer
+                
+                jmp input_number
+
+            already_marked:
+                ; display prompt and go back to menu
+                push already_marked_msg
+                call _printf
+                add esp, 4
+
+                jmp main_loop_start
 
         case_4:
+
 
 clear_buffer:
     clear_input_buffer:
@@ -182,7 +240,7 @@ append_new_task:
     mov ebp, esp
     
     ; check if maximum number of task is reached
-    cmp ecx, MAX_TASKS
+    cmp dword[task_count], MAX_TASKS
     jne proceed_append
 
     ; display task limit message
@@ -198,19 +256,15 @@ append_new_task:
     mul ecx
     lea edi, [task_list + eax] ; address of task_list as destination
     mov esi, [ebp + 4] ; address of task as source
-    
     add esi, 1 ; exclude the newline at the beginning
     
-    push ecx ; preserve ecx value to stack
-
     mov ecx, TASK_SIZE
     rep movsb ; mov the content from source to destination
     
-    pop ecx ; restore ecx value
-
     ; Mark task as pending (0)
-    mov byte [task_status + ecx], 0
-    inc dword [task_count]
+    mov ecx, dword[task_count] ; current number of task
+    mov byte[task_status + ecx], 0
+    inc byte[task_count]
 
     ; print success message
     push task_added_msg
@@ -226,30 +280,24 @@ display_all_task:
     call _printf
     add esp, 4
 
-    mov ebx, 1
+    mov ebx, 0
     lea esi, [task_list]
 
     display_loop_start:
         cmp ebx, MAX_TASKS
-        jg display_done
+        jge display_done
 
-        push esi
-        mov eax, 0
-        lodsb ; store first character in esi to al
-        pop esi
-
-        cmp al, 0
+        ; check if task list is empty
+        cmp byte[esi], 0
         je display_empty
 
         ; check for task status
-        dec ebx
-        mov eax, [task_status + ebx] 
-        inc ebx
-        cmp eax, 0
+        cmp byte[task_status + ebx], 0
         je display_pending
 
         ; display completed tasks
         push esi
+        inc ebx
         push ebx
         push completed_status
         call _printf
@@ -260,6 +308,7 @@ display_all_task:
         display_pending:
             ; display pending tasks
             push esi
+            inc ebx
             push ebx
             push pending_status
             call _printf
@@ -269,13 +318,13 @@ display_all_task:
 
         display_empty:
             ; display pending tasks
+            inc ebx
             push ebx
             push no_display_format
             call _printf
             add esp, 8
 
         next_display:
-        inc ebx
         add esi, TASK_SIZE ; move to the next task
 
         jmp display_loop_start
